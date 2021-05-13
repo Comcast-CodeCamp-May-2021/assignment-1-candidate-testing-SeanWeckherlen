14c07f6adf7107ff6d3b0d16267e546ac1f197c8
Sean Weckherlen <>
2021-05-13 15:03:06 +0000

    attempt pearce 5

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = '';
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometers == 5000 meters ? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometers == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 let numberOfCorrectAnswer = 0;
@@ -28,8 +28,9 @@ function askQuestion() {
   }
 }
 
-function gradeQuiz() {
+function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly 
+
 for (let i = 0; i < candidateAnswers.length; i++){
   if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
       numberOfCorrectAnswer += 1;
@@ -52,7 +53,7 @@ function runProgram() {
   console.log(`Welcoome ${canidateName} !`)
   
   askQuestion();
-  gradeQuiz();
+  gradeQuiz(this.candidateAnswers);
   console.log("\n");
   console.log(
 `Candidate Name : ${canidateName}\n
