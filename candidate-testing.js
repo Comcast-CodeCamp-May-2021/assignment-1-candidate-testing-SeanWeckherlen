7fc7c52f1b77a0d5120bb29f5e18fadc8cdf0615
Sean Weckherlen <>
2021-05-13 15:28:22 +0000

    attempt 8

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,9 @@ let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
-let numberOfCorrectAnswer = 0;
-let status; 
-let grade; 
+// let numberOfCorrectAnswer = 0;
+// let status; 
+// let grade; 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -30,8 +30,10 @@ function askQuestion() {
 
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly 
-
-for (let i = 0; i < candidateAnswers.length; i++){
+  let numberOfCorrectAnswer = 0;
+  let status; 
+  let grade; 
+  for (let i = 0; i < candidateAnswers.length; i++){
   if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
       numberOfCorrectAnswer += 1;
     }  
@@ -43,18 +45,7 @@ for (let i = 0; i < candidateAnswers.length; i++){
   }else{
     status = "FAILED";
   }
-  
-  return grade;
-}
-
-function runProgram() {
-  askForName();
-  // TODO 1.1c: Ask for candidate's name //
-  console.log(`Welcoome ${canidateName} !`)
-  
-  askQuestion();
-  gradeQuiz(this.candidateAnswers);
-  console.log("\n");
+ console.log("\n");
   console.log(
 `Candidate Name : ${canidateName}\n
 1) ${questions[0]} 
@@ -75,6 +66,38 @@ Correct Answer : ${correctAnswers[4]}\n
 >>> Overall Grade: ${grade}% (${numberOfCorrectAnswer} of 5 responses correct) <<<
 >>> Status: ${status} <<<
   `);
+
+  return grade;
+}
+
+function runProgram() {
+  askForName();
+  // TODO 1.1c: Ask for candidate's name //
+  console.log(`Welcoome ${canidateName} !`)
+  
+  askQuestion();
+  gradeQuiz(this.candidateAnswers);
+  //console.log("\n");
+  //console.log(
+// `Candidate Name : ${canidateName}\n
+// 1) ${questions[0]} 
+// Your Answer: ${candidateAnswers[0]}
+// Correct Answer : ${correctAnswers[0]} \n 
+// 2) ${questions[1]}
+// Your Answer : ${candidateAnswers[1]}
+// Correct Answer : ${correctAnswers[1]}\n  
+// 3) ${questions[2]}
+// Your Answer : ${candidateAnswers[2]}
+// Correct Answer : ${correctAnswers[2]}\n 
+// 4) ${questions[3]}
+// Your Answer : ${candidateAnswers[3]} 
+// Correct Answer : ${correctAnswers[3]}\n 
+// 5) ${questions[4]}
+// Your Answer : ${candidateAnswers[4]}
+// Correct Answer : ${correctAnswers[4]}\n
+// >>> Overall Grade: ${grade}% (${numberOfCorrectAnswer} of 5 responses correct) <<<
+// >>> Status: ${status} <<<
+//   `);
 }
 
 
