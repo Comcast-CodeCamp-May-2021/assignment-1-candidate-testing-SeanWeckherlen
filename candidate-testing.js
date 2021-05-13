39f03de6cd6fb8f559ce35c99272741a823227ba
Sean Weckherlen <>
2021-05-13 18:09:41 +0000

    FINAL EDIT

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,6 @@ let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
-// let numberOfCorrectAnswer = 0;
-// let status; 
-// let grade; 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -31,40 +28,34 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly 
   let numberOfCorrectAnswer = 0;
-  
+  let status;
+
   for (let i = 0; i < candidateAnswers.length; i++){
-  if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
       numberOfCorrectAnswer += 1;
     }  
   }
-  grade = (numberOfCorrectAnswer / 5) * 100;
+  let grade = (numberOfCorrectAnswer / 5) * 100;
   
   if (grade >= 80){
     status = "PASSED";
   }else{
     status = "FAILED";
   }
- console.log("\n");
+  
   console.log(
-`Candidate Name : ${candidateName}\n
-1) ${questions[0]} 
-Your Answer: ${candidateAnswers[0]}
-Correct Answer : ${correctAnswers[0]} \n 
-2) ${questions[1]}
-Your Answer : ${candidateAnswers[1]}
-Correct Answer : ${correctAnswers[1]}\n  
-3) ${questions[2]}
-Your Answer : ${candidateAnswers[2]}
-Correct Answer : ${correctAnswers[2]}\n 
-4) ${questions[3]}
-Your Answer : ${candidateAnswers[3]} 
-Correct Answer : ${correctAnswers[3]}\n 
-5) ${questions[4]}
-Your Answer : ${candidateAnswers[4]}
-Correct Answer : ${correctAnswers[4]}\n
+  `\nCandidate Name : ${candidateName}`)
+
+  for (let i = 0; i < 5; i++) {
+  console.log(`
+${i+1}) ${questions[i]}
+Your Answer : ${candidateAnswers[i]}
+Correct Answer : ${correctAnswers[i]}`)
+  }
+  console.log(`
 >>> Overall Grade: ${grade}% (${numberOfCorrectAnswer} of 5 responses correct) <<<
->>> Status: ${status} <<<
-  `);
+>>> Status: ${status} <<<"`)
+
   return grade;
 }
 
@@ -75,27 +66,6 @@ function runProgram() {
   
   askQuestion();
   gradeQuiz(this.candidateAnswers);
-  //console.log("\n");
-  //console.log(
-// `Candidate Name : ${canidateName}\n
-// 1) ${questions[0]} 
-// Your Answer: ${candidateAnswers[0]}
-// Correct Answer : ${correctAnswers[0]} \n 
-// 2) ${questions[1]}
-// Your Answer : ${candidateAnswers[1]}
-// Correct Answer : ${correctAnswers[1]}\n  
-// 3) ${questions[2]}
-// Your Answer : ${candidateAnswers[2]}
-// Correct Answer : ${correctAnswers[2]}\n 
-// 4) ${questions[3]}
-// Your Answer : ${candidateAnswers[3]} 
-// Correct Answer : ${correctAnswers[3]}\n 
-// 5) ${questions[4]}
-// Your Answer : ${candidateAnswers[4]}
-// Correct Answer : ${correctAnswers[4]}\n
-// >>> Overall Grade: ${grade}% (${numberOfCorrectAnswer} of 5 responses correct) <<<
-// >>> Status: ${status} <<<
-//   `);
 }
 
 
